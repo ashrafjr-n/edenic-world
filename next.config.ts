8ad609a941ee20718f4483fb962ead4162582fe5
Ashraf <250116877+ashrafjr-n@users.noreply.github.com>
2026-08-20 17:06:59 +0400

    allow next/image to optimize YouTube thumbnails V1.0

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.join(__dirname),
   },
+  images: {
+    remotePatterns: [new URL("https://img.youtube.com/vi/**")],
+  },
 };
 
 export default nextConfig;
