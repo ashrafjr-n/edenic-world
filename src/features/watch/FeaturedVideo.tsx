52cf75d4c2157d376c5a8975629c6736160f2e59
Ashraf <250116877+ashrafjr-n@users.noreply.github.com>
2026-08-20 22:06:13 +0400

    featured video prefers custom artwork over youtube thumbnail V1.4

--- src/features/watch/FeaturedVideo.tsx
+++ src/features/watch/FeaturedVideo.tsx
@@ -40,7 +40,7 @@ export default function FeaturedVideo({ video }: { video: Video }) {
                     className="absolute inset-0 size-full"
                   />
                   <Image
-                    src={`https://img.youtube.com/vi/${video.youtubeId}/hqdefault.jpg`}
+                    src={video.image ?? `https://img.youtube.com/vi/${video.youtubeId}/hqdefault.jpg`}
                     alt=""
                     fill
                     sizes="(min-width: 1024px) 60vw, 100vw"
